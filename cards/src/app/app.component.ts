0d77cde75244c40c31b7164c634eb7ebc012e990
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-05 10:36:09 +0100

    basic component rendering logic

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // must import component here for child to render
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cards';
