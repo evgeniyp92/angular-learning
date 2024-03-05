2b4e3fec19276883292fb4ad34488916ef883ae7
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-05 16:07:24 +0100

    implement ngFor
    like array.map() for angular

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// for ngFor to work this below MUST be imported and declared in the decorator
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 
@@ -6,7 +8,7 @@ import { CardComponent } from './card/card.component';
   selector: 'app-root',
   standalone: true,
   // must import component here for child to render
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
