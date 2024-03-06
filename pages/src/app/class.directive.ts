bd3b5c392636e7b6a571fa2a7827bf61b23c64e6
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 15:06:10 +0100

    Refactor appClass directive to accept background color input

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
   standalone: true,
 })
 export class ClassDirective {
-  // setting up the constructor to bind and provide the element it was applied to
+  @Input() backgroundColor: string = '';
+  // setting up the constructor to bind and provide the element it was applied
+  // to current implementation wont work due to a race condition (element is not
+  // yet available when the directive is applied to the element)
   constructor(private element: ElementRef) {
-    console.log(`class directive used`);
-    this.element.nativeElement.style.backgroundColor = 'orange';
+    // never do this!!! this is a hack
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 500);
   }
 }
