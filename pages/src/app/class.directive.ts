a89f1fdc3a82410439f41380732fd70ca9513b04
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 15:23:29 +0100

    Make correct custom directive implementation via setter

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -5,14 +5,14 @@ import { Directive, ElementRef, Input } from '@angular/core';
   standalone: true,
 })
 export class ClassDirective {
-  @Input() backgroundColor: string = '';
   // setting up the constructor to bind and provide the element it was applied
   // to current implementation wont work due to a race condition (element is not
   // yet available when the directive is applied to the element)
-  constructor(private element: ElementRef) {
-    // never do this!!! this is a hack
-    setTimeout(() => {
-      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
-    }, 500);
+  constructor(private element: ElementRef) {}
+
+  // setting up the getter and setter for the backgroundColor property and using
+  // the input decorator to bind the property to the directive
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 }
