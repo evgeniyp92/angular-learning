284e95ccd55d1fd66cc63f1d88557db9f385970d
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 14:59:05 +0100

    Add custom directive to toggle class and set background color

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClassDirective } from './class.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ClassDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -1,11 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
   standalone: true,
 })
 export class ClassDirective {
-  constructor() {
+  // setting up the constructor to bind and provide the element it was applied to
+  constructor(private element: ElementRef) {
     console.log(`class directive used`);
+    this.element.nativeElement.style.backgroundColor = 'orange';
   }
 }
