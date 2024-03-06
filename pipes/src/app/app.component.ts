ab5bc935d07f8b80f27185b8f88018d63f9c1cd6
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 09:33:50 +0100

    write fields and implement pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,22 +1,36 @@
 // Pipes are functions that transform data. They are used in the template to
 // format data before displaying it.
-import { TitleCasePipe } from '@angular/common';
+import { TitleCasePipe, DatePipe, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   public name: string = '';
+  public date: string = '';
+  public amount: number = 0;
 
   onNameChange(eventTarget: EventTarget | null) {
     if (eventTarget instanceof HTMLInputElement) {
       this.name = eventTarget.value;
+    }
+  }
+
+  onDateChange(eventTarget: EventTarget | null) {
+    if (eventTarget instanceof HTMLInputElement) {
+      this.date = eventTarget.value;
+    }
+  }
+
+  onAmountChange(eventTarget: EventTarget | null) {
+    if (eventTarget instanceof HTMLInputElement) {
+      this.amount = parseFloat(eventTarget.value);
     }
   }
 }
