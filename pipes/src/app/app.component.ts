3c84e988f2f252c2ac544b05fbd9f3589e93a15d
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 10:46:41 +0100

    add number pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,13 +1,18 @@
 // Pipes are functions that transform data. They are used in the template to
 // format data before displaying it.
-import { TitleCasePipe, DatePipe, CurrencyPipe } from '@angular/common';
+import {
+  TitleCasePipe,
+  DatePipe,
+  CurrencyPipe,
+  DecimalPipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,6 +20,7 @@ export class AppComponent {
   public name: string = '';
   public date: string = '';
   public amount: number = 0;
+  public height: number = 0;
 
   onNameChange(eventTarget: EventTarget | null) {
     if (eventTarget instanceof HTMLInputElement) {
@@ -31,6 +37,12 @@ export class AppComponent {
   onAmountChange(eventTarget: EventTarget | null) {
     if (eventTarget instanceof HTMLInputElement) {
       this.amount = parseFloat(eventTarget.value);
+    }
+  }
+
+  onHeightChange(eventTarget: EventTarget | null) {
+    if (eventTarget instanceof HTMLInputElement) {
+      this.height = parseFloat(eventTarget.value);
     }
   }
 }
