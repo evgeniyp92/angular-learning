934ac7aa251dbb1d2d2f9e913e0f54c59b8f9646
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 17:14:50 +0100

    add explanations of NgModule props

--- comps/src/app/collections/collections.module.ts
+++ comps/src/app/collections/collections.module.ts
@@ -5,8 +5,10 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CollectionsRoutingModule, CollectionsHomeComponent],
-  exports: [CollectionsHomeComponent],
+  declarations: [], // components, pipes, directives made in the module
+  imports: [CommonModule, CollectionsRoutingModule, CollectionsHomeComponent], // dependencies of the module
+  exports: [CollectionsHomeComponent], // components, pipes, directives made available to other modules
+  // providers: [], // services made available to the module [OLD]
+  // bootstrap: [], // root component of the module [OLD]
 })
 export class CollectionsModule {}
