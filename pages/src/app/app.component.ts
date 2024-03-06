8474dd4f30355158cec5489deed5378468a1a452
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 14:40:19 +0100

    Remove unused import NgFor in app.component.ts

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
