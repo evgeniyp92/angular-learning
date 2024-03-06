529f68f909376c99ca7cb4de5a8c14f2653ff488
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 16:27:36 +0100

    structural directive example

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ClassDirective],
+  imports: [RouterOutlet, CommonModule, ClassDirective, TimesDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- pages/src/app/times.directive.ts
+++ pages/src/app/times.directive.ts
@@ -1,11 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTimes]',
-  standalone: true
+  standalone: true,
 })
 export class TimesDirective {
+  // viewContainer is what the directive was applied to
+  // templateRef is the elements to be placed inside of the directive
+  constructor(
+    private viewContainer: ViewContainerRef, // lets us easily append elements to the DOM
+    private templateRef: TemplateRef<any>
+  ) {}
 
-  constructor() { }
-
+  @Input('appTimes') set render(times: number) {
+    // purge the viewContainer of any elements
+    this.viewContainer.clear();
+    // create a new embedded view for each time
+    for (let i = 0; i < times; i++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, {});
+    }
+  }
 }
