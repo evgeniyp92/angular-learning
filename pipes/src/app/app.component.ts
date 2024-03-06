60f0fd503bb4e1bf9e813f54de19e7a6e8574374
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 09:02:15 +0100

    Add payment form and titlecase pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,13 +1,22 @@
+// Pipes are functions that transform data. They are used in the template to
+// format data before displaying it.
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleCasePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pipes';
+  public name: string = '';
+
+  onNameChange(eventTarget: EventTarget | null) {
+    if (eventTarget instanceof HTMLInputElement) {
+      this.name = eventTarget.value;
+    }
+  }
 }
