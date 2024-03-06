2c564ec9af7b616dbd9ff492e4cececfea3b8b42
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 18:03:20 +0100

    scaffold and wire up home and notfound components

--- comps-no-standalone/src/app/app.module.ts
+++ comps-no-standalone/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent], // list components pipes, directives created in a module
+  declarations: [AppComponent, HomeComponent, NotFoundComponent], // list components pipes, directives created in a module
   imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule], // list dependencies of the module
   exports: [], // list what is made available to other modules
   providers: [], // old way of connecting modules and services
