f1d54d634a9c2bf6d0589dbfc79cf98883a7187a
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 13:53:51 +0100

    initial project commit

---
+++ pages/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  public images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206',
+    },
+    {
+      title: 'At the Park',
+      url: 'https://images.unsplash.com/photo-1519331379826-f10be5486c6f',
+    },
+    {
+      title: 'At the Mountain',
+      url: 'https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5',
+    },
+  ];
+}
