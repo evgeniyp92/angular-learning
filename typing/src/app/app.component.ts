29716d89f33e913ad8e2ba4f657973e8dd3f9e44
Evgeniy Pimenov <72388999+evgeniyp92@users.noreply.github.com>
2024-03-06 08:14:15 +0100

    start implementing typing challenge

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing';
+  public randomText: string = 'Lorem ipsum dolor sit amet';
+  private enteredText: string = '';
+
+  ngOnInit() {
+    // call the faker function to generate a random name
+    const name = faker.lorem.sentence();
+    this.randomText = name;
+  }
+
+  public inputMatchesString() {
+    return this.enteredText === this.randomText;
+  }
+
+  public handleInput(eventTarget: EventTarget | null) {
+    // If the event target is an input element, log the value
+    if (eventTarget instanceof HTMLInputElement) {
+      this.enteredText = eventTarget.value;
+    }
+    // otherwise do nothing
+  }
 }
