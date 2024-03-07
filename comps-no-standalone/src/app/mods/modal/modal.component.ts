ecc03d90e257c4a21ed0c9a0346df490e9256426
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-07 13:13:52 +0100

    add comments about ngOnInit()

--- comps-no-standalone/src/app/mods/modal/modal.component.ts
+++ comps-no-standalone/src/app/mods/modal/modal.component.ts
@@ -1,15 +1,17 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   // getting the root dom element thru dependency injection
   constructor(private el: ElementRef) {}
 
-  // appending the modal to the root dom element
+  // appending the modal to the root dom element ngOnInit is a lifecycle hook
+  // that is called once after the component is first displayed on screen and
+  // after Angular has set props passed from parent component
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
   }
