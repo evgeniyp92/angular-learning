9986f266ade7cce5827a3d9d7c7f85e9216531b3
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-07 13:30:17 +0100

    implement button to show modal

--- comps-no-standalone/src/app/mods/mods-home/mods-home.component.ts
+++ comps-no-standalone/src/app/mods/mods-home/mods-home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrl: './mods-home.component.css'
+  styleUrl: './mods-home.component.css',
 })
 export class ModsHomeComponent {
+  modalOpen = false;
 
+  onClick() {
+    this.modalOpen = !this.modalOpen;
+  }
 }
