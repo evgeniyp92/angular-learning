bc212f31fcf7645e58a4d585e613b452794ba06b
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-07 14:46:07 +0100

    finish writing accordion

--- comps-no-standalone/src/app/mods/accordion/accordion.component.ts
+++ comps-no-standalone/src/app/mods/accordion/accordion.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrl: './accordion.component.css'
+  styleUrl: './accordion.component.css',
 })
 export class AccordionComponent {
+  @Input() items: { title: string; content: string }[] = [];
+  openedItemIndex: number = 0;
 
+  setOpenedItem(index: number) {
+    this.openedItemIndex = index;
+  }
 }
--- comps-no-standalone/src/app/mods/mods-home/mods-home.component.ts
+++ comps-no-standalone/src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ModsHomeComponent {
   modalOpen = false;
+  items = [
+    {
+      title: 'Why is the sky blue?',
+      content: 'The sky is blue because it is.',
+    },
+    {
+      title: 'What does an orange taste like?',
+      content: 'An orange tastes like an orange.',
+    },
+    { title: 'What color is the cat?', content: 'The cat is black.' },
+  ];
 
   onClick() {
     this.modalOpen = !this.modalOpen;
