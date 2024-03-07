e05a59941f365c176fe70ac285fb424d7b0560ba
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-07 09:05:36 +0100

    implement lazy loading for collections

--- comps-no-standalone/src/app/app.module.ts
+++ comps-no-standalone/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent], // list components pipes, directives created in a module
   // order of imports matters for routing, FIFO principle
-  imports: [BrowserModule, CollectionsModule, AppRoutingModule], // list dependencies of the module
+  imports: [BrowserModule, AppRoutingModule], // list dependencies of the module
   exports: [], // list what is made available to other modules
   providers: [], // old way of connecting modules and services
   bootstrap: [AppComponent], // used by AppModule only to declare what component to start with
