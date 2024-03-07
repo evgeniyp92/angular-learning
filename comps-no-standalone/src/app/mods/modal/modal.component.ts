c45e37814199442c62bafd5e8b1798de72b0cbbd
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-07 13:04:28 +0100

    create modal in dom root (aka portal)

--- comps-no-standalone/src/app/mods/modal/modal.component.ts
+++ comps-no-standalone/src/app/mods/modal/modal.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  // getting the root dom element thru dependency injection
+  constructor(private el: ElementRef) {}
 
+  // appending the modal to the root dom element
+  ngOnInit() {
+    document.body.appendChild(this.el.nativeElement);
+  }
 }
