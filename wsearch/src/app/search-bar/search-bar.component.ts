f3c5ccd5779cb5d7c10e7c9dfb602dbb69cd3ed0
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-08 09:42:31 +0100

    set up basic event handler

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+  term = '';
 
+  onInput(event: Event) {
+    this.term = (event.target as HTMLInputElement).value;
+  }
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log(this.term);
+  }
 }
