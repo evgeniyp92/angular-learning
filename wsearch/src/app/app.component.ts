e80f1fb0f9b681cabe519ded3b3ff0ab7e32aab1
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-08 10:26:32 +0100

    explain how services work in angular

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -7,7 +7,13 @@ import { WikipediaService } from './wikipedia.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // inject service depenency
+  // inject service depenency in this case, the 'wikipedia' will be added as a
+  // private prop automatically to the instance of the app component, and will
+  // be of type 'WikipediaService'. This is a shorthand way of creating a
+  // private prop and assigning it the value of the argument passed to the
+  // constructor. This is a feature of TypeScript, not Angular.
+
+  // TLDR any service can be made available in any component by adding it to the constructor
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,6 +1,11 @@
 // services are automatically made available to all parts of your app
 import { Injectable } from '@angular/core';
 
+// The @Injectable decorator is used to define a service. Since it's provided in
+// root, it will be available to all parts of the app. The providedIn property is
+// used to register the service with the root injector. This is a shorthand way
+// of registering the service with the root injector. This is a feature of
+// Angular, not TypeScript.
 @Injectable({
   providedIn: 'root',
 })
