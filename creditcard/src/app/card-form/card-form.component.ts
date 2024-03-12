1c586113c09e3112abbfa1f1a155c2c9d4939a80
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-12 13:52:16 +0100

    write reusable input

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -10,7 +10,12 @@ export class CardFormComponent {
   cardForm = new FormGroup({
     // The Validators class is used for form validation.
     // NB: Validators are ran left to right and when there are no errors the value is null
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      // Validators.maxLength(24),
+      // Validators.pattern(/\\s/),
+    ]),
     cardNumber: new FormControl(''),
     expiration: new FormControl(''),
     cvv: new FormControl(''),
