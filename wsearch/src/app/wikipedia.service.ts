668ff4c5d1e90545de7cc6646338808181562ecb
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-12 10:28:38 +0100

    write some rxjs+ts implementation

--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -2,6 +2,32 @@
 import { Injectable } from '@angular/core';
 // The HttpClient module is used to make HTTP requests
 import { HttpClient } from '@angular/common/http';
+import { Observable, pluck } from 'rxjs'; // 30k gzipped :o
+
+interface Car {
+  year: number;
+  color: string;
+  running: boolean;
+  make: {
+    name: string;
+    dateCreated: number;
+  };
+}
+
+// Observable class can get a type arg to specify the type of data it will emit
+const observable = new Observable<Car>((observer) => {
+  observer.next({
+    color: 'red',
+    year: 2000,
+    running: true,
+    make: { name: 'Ford', dateCreated: 2000 },
+  });
+}).pipe(pluck('make', 'dateCreated'));
+
+// Typescript is aware enough to know what kind of data is emitted at the end of the pipe
+observable.subscribe((value) => {
+  console.log(value);
+});
 
 // The @Injectable decorator is used to define a service. Since it's provided in
 // root, it will be available to all parts of the app. The providedIn property is
@@ -16,6 +42,7 @@ export class WikipediaService {
 
   search(term: string) {
     // this returns an observable
+    // observables are the source of a lot of bugs in Angular
     return this.http.get('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
