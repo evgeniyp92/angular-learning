2e9c297b110bed058b090abbf557734613e5c2d2
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-13 10:02:02 +0100

    scaffold form

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
   styleUrl: './equation.component.css',
 })
-export class EquationComponent {}
+export class EquationComponent {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber),
+    b: new FormControl(this.randomNumber),
+    answer: new FormControl(''),
+  });
+
+  get randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+}
