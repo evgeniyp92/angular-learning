aff5b2d4067d69f55653f0337e8e706a6393633e
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-13 09:54:31 +0100

    initial commit for mathform

---
+++ mathform/src/app/equation/equation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrl: './equation.component.css',
+})
+export class EquationComponent {}
