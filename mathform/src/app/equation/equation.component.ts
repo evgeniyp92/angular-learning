9e7f3558764d4ab8a40ef98c59654271d3583177
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-13 10:10:46 +0100

    write custom validator

--- mathform/src/app/equation/equation.component.ts
+++ mathform/src/app/equation/equation.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -7,11 +12,22 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrl: './equation.component.css',
 })
 export class EquationComponent {
-  mathForm = new FormGroup({
-    a: new FormControl(this.randomNumber),
-    b: new FormControl(this.randomNumber),
-    answer: new FormControl(''),
-  });
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber),
+      b: new FormControl(this.randomNumber),
+      answer: new FormControl(''),
+    },
+    [
+      (form: AbstractControl): ValidationErrors | null => {
+        const { a, b, answer } = form.value;
+        if (a + b !== parseInt(answer)) {
+          return { addition: true }; // error case
+        }
+        return null; // success case
+      },
+    ]
+  );
 
   get randomNumber() {
     return Math.floor(Math.random() * 10);
