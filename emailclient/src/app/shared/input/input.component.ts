abde7cf4f2a58adfbb3244b39ba3bc100a491d0a
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-14 09:05:32 +0100

    set up basic input component template

--- emailclient/src/app/shared/input/input.component.ts
+++ emailclient/src/app/shared/input/input.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
 })
 export class InputComponent {
-
+  @Input() label: string = '';
+  @Input() control: AbstractControl | null = new FormControl();
 }
