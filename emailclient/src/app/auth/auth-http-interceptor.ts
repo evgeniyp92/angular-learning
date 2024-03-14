97eecfc8d3f810860acd1efd7626aa65fd1c24ea
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-14 12:13:26 +0100

    write more interceptor logic

--- emailclient/src/app/auth/auth-http-interceptor.ts
+++ emailclient/src/app/auth/auth-http-interceptor.ts
@@ -4,8 +4,9 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpEventType,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, filter } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthHttpInterceptor implements HttpInterceptor {
@@ -13,12 +14,21 @@ export class AuthHttpInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    // Modify or log the outgoing request here
+
     // req.withCredentials = true; // doesnt work, props are read-only
     // const newReq = { ...req, withCredentials: true }; // doesnt work, missing serialize method
     const newReq = req.clone({
       withCredentials: true,
     });
     console.log(newReq);
-    return next.handle(newReq); // handing off the http request to the next interceptor or the actual request handler
+    // handing off the http request to the next interceptor or the actual request handler
+    // this is actually an observable so we can listen to it after we hand it off
+    return next.handle(newReq).pipe(
+      filter((v) => v.type === HttpEventType.Sent),
+      tap((v) => {
+        console.log('Request was sent to server');
+      })
+    );
   }
 }
