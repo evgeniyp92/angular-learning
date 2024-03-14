59746ebf887da4e9fd559bf59ba14d13a1b957e5
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-14 12:52:53 +0100

    write signout handler

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -62,4 +62,12 @@ export class AuthService {
       })
     );
   }
+
+  signout() {
+    return this.http.post(this.rootUrl + 'auth/signout', {}).pipe(
+      tap(() => {
+        this.signedIn$.next(false);
+      })
+    );
+  }
 }
